cfb50dd697572bb28232c94831d2b3f3a1ec1e6a
[Avery-Hantla] <[averyshantla@gmail.com]>
2025-04-03 13:31:24 -0500

    Fixed bug with residual

--- code/res.cpp
+++ code/res.cpp
@@ -6,18 +6,18 @@
 #include "class_q.hpp"
 #include "class_mesh.hpp"
 #include "class_f.hpp"
+#include "class_residual.hpp"
 #include "struct_size.hpp"
 #include "struct_inputs.hpp"
 #include "class_flow.hpp"
 #include "struct_BC.hpp"
-#include "struct_residual.hpp"
 
 #include "reconstruction.cpp"
 #include "rusanov.cpp"
 
-void res(struct_residual* residual, class_mesh* mesh, struct_size* size, struct_inputs* inputs, class_Q* Qbar, class_Q* Qface_c1, class_Q* Qface_c2, class_flow* freestream, struct_BC* BC) {
+void res(class_residual* residual, class_mesh* mesh, struct_size* size, struct_inputs* inputs, class_Q* Qbar, class_Q* Qface_c1, class_Q* Qface_c2, class_flow* freestream, struct_BC* BC) {
     class_F F_reimann;
-    F_reimann.init(size->num_cells*6, freestream->gamma);
+    F_reimann.init(size->num_faces, freestream->gamma);
 
     // Reconstuct Cell Faces
     reconstruction(mesh, Qbar, Qface_c1, Qface_c2, freestream, size, inputs, BC);
@@ -25,20 +25,35 @@ void res(struct_residual* residual, class_mesh* mesh, struct_size* size, struct_
     rusanov(&F_reimann, Qface_c1, Qface_c2, mesh, freestream, size, BC); 
 
     // Compute Residual
-    int face_num;
-    for (int idx = 0; idx < size->num_cells; idx++) {
+    int c1_num, c2_num;
+    residual->reset();
+    for (int idx = 0; idx < size->num_faces; idx++) {
         double sum_temp1 = 0, sum_temp2 = 0, sum_temp3 = 0, sum_temp4 = 0;
-        for (int jdx = 0; jdx < mesh->cell_face_count[idx]; jdx ++) {
-            face_num = mesh->find_cell_face(idx,jdx);
 
-            sum_temp1 += mesh->get_cell_face_rx(idx,jdx)*(F_reimann.p1[face_num]*mesh->face_area[face_num]);
-            sum_temp2 += mesh->get_cell_face_rx(idx,jdx)*(F_reimann.p2[face_num]*mesh->face_area[face_num]);
-            sum_temp3 += mesh->get_cell_face_rx(idx,jdx)*(F_reimann.p3[face_num]*mesh->face_area[face_num]);
-            sum_temp4 += mesh->get_cell_face_rx(idx,jdx)*(F_reimann.p4[face_num]*mesh->face_area[face_num]);
+        c1_num = mesh->face_cell1[idx];
+        c2_num = mesh->face_cell2[idx];
+
+        sum_temp1 = (F_reimann.p1[idx]*mesh->face_area[idx]);
+        sum_temp2 = (F_reimann.p2[idx]*mesh->face_area[idx]);
+        sum_temp3 = (F_reimann.p3[idx]*mesh->face_area[idx]);
+        sum_temp4 = (F_reimann.p4[idx]*mesh->face_area[idx]);
+
+        residual->p1[c1_num] -= sum_temp1;
+        residual->p2[c1_num] -= sum_temp2;
+        residual->p3[c1_num] -= sum_temp3;
+        residual->p4[c1_num] -= sum_temp4;
+
+        if (c2_num >= 0) {
+            residual->p1[c2_num] += sum_temp1;
+            residual->p2[c2_num] += sum_temp2;
+            residual->p3[c2_num] += sum_temp3;
+            residual->p4[c2_num] += sum_temp4;
         }
-        residual->p1[idx] = (-1/mesh->cell_vol[idx]) * sum_temp1;
-        residual->p2[idx] = (-1/mesh->cell_vol[idx]) * sum_temp2;
-        residual->p3[idx] = (-1/mesh->cell_vol[idx]) * sum_temp3;
-        residual->p4[idx] = (-1/mesh->cell_vol[idx]) * sum_temp4;
+    }
+    for (int idx = 0; idx < size->num_cells; idx ++) {
+        residual->p1[idx] = residual->p1[idx] / mesh->cell_vol[idx];
+        residual->p2[idx] = residual->p2[idx] / mesh->cell_vol[idx];
+        residual->p3[idx] = residual->p3[idx] / mesh->cell_vol[idx];
+        residual->p4[idx] = residual->p4[idx] / mesh->cell_vol[idx];
     }
 }
