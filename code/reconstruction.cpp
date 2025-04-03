195f94a726d11c175ac28b9d02406f1d332162ef
[Avery-Hantla] <[averyshantla@gmail.com]>
2025-04-03 13:34:05 -0500

    Updated BC

--- code/reconstruction.cpp
+++ code/reconstruction.cpp
@@ -27,41 +27,50 @@ void reconstruction(class_mesh* mesh, class_Q* Qbar, class_Q* Qfaces_c1, class_Q
 
                 cell_2 = mesh->face_cell2[idx];
                 // Apply BC
-                if (cell_2 == -1) { // Freestream BC
-                    Qfaces_c2->p1[idx] = freestream->Q1;
-                    Qfaces_c2->p2[idx] = freestream->Q2;
-                    Qfaces_c2->p3[idx] = freestream->Q3;
-                    Qfaces_c2->p4[idx] = freestream->Q4;
-                } else if (cell_2 == -2) { // Wall BC
-                    wall_BC.rho = Qbar->rho[cell_2];
-                    wall_BC.P = Qbar->P[cell_2];
-                    Vn = Qbar->u[cell_2]*mesh->face_nx[idx] + Qbar->v[cell_2]*mesh->face_ny[idx];
-                    wall_BC.u = Qbar->u[cell_2] - 2*Vn*mesh->face_nx[idx];
-                    wall_BC.v = Qbar->v[cell_2] - 2*Vn*mesh->face_ny[idx];
-                    wall_BC.updateQ();
+                switch (cell_2) { 
+                    case -1: // Freestream BC
+                        Qfaces_c2->p1[idx] = freestream->Q1;
+                        Qfaces_c2->p2[idx] = freestream->Q2;
+                        Qfaces_c2->p3[idx] = freestream->Q3;
+                        Qfaces_c2->p4[idx] = freestream->Q4;
+                        break;
+                    case -2:  // Wall BC
+                        wall_BC.rho = Qbar->rho[cell_1];
+                        wall_BC.P = Qbar->P[cell_1];
+                        Vn = Qbar->u[cell_1]*mesh->face_nx[idx] + Qbar->v[cell_1]*mesh->face_ny[idx];
+                        wall_BC.u = Qbar->u[cell_1] - 2.0*Vn*mesh->face_nx[idx];
+                        wall_BC.v = Qbar->v[cell_1] - 2.0*Vn*mesh->face_ny[idx];
+                        wall_BC.updateQ();
 
-                    Qfaces_c2->p1[idx] = wall_BC.Q1;
-                    Qfaces_c2->p2[idx] = wall_BC.Q2;
-                    Qfaces_c2->p3[idx] = wall_BC.Q3;
-                    Qfaces_c2->p4[idx] = wall_BC.Q4;
-                } else if (cell_2 == -3) { // Extrapolation BC
-                    Qfaces_c2->p1[idx] = Qbar->p1[cell_1];
-                    Qfaces_c2->p2[idx] = Qbar->p2[cell_1];
-                    Qfaces_c2->p3[idx] = Qbar->p3[cell_1];
-                    Qfaces_c2->p4[idx] = Qbar->p4[cell_1];
-                } else {
-                    Qfaces_c2->p1[idx] = Qbar->p1[cell_2];
-                    Qfaces_c2->p2[idx] = Qbar->p2[cell_2];
-                    Qfaces_c2->p3[idx] = Qbar->p3[cell_2];
-                    Qfaces_c2->p4[idx] = Qbar->p4[cell_2];
+                        Qfaces_c2->p1[idx] = wall_BC.Q1;
+                        Qfaces_c2->p2[idx] = wall_BC.Q2;
+                        Qfaces_c2->p3[idx] = wall_BC.Q3;
+                        Qfaces_c2->p4[idx] = wall_BC.Q4;
+                        break;
+                    case -3: // Extrapolation BC
+                        Qfaces_c2->p1[idx] = Qbar->p1[cell_1];
+                        Qfaces_c2->p2[idx] = Qbar->p2[cell_1];
+                        Qfaces_c2->p3[idx] = Qbar->p3[cell_1];
+                        Qfaces_c2->p4[idx] = Qbar->p4[cell_1];
+                        break;
+                    default:
+                        Qfaces_c2->p1[idx] = Qbar->p1[cell_2];
+                        Qfaces_c2->p2[idx] = Qbar->p2[cell_2];
+                        Qfaces_c2->p3[idx] = Qbar->p3[cell_2];
+                        Qfaces_c2->p4[idx] = Qbar->p4[cell_2];
+                        break;
                 }
             }
             Qfaces_c1->updateflow();
             Qfaces_c2->updateflow();
             break;
+
+
         case 2: // Second Order FV Scheme
             
+
+
         default:
-            std::cout << "ERROR: ORDER NOT IMPLEMENTED";
+            std::cout << "ERROR: ORDER NOT IMPLEMENTED \n";
     };
 }
