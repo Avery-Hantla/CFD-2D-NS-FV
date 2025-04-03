9ba4249d405ee9d53e188ec06a0b2638008f7877
[Avery-Hantla] <[averyshantla@gmail.com]>
2025-04-03 15:20:03 -0500

    fixed time stepping bug

--- code/ssp_rk2.cpp
+++ code/ssp_rk2.cpp
@@ -7,29 +7,34 @@
 #include "class_q.hpp"
 #include "class_mesh.hpp"
 #include "class_flow.hpp"
+#include "class_residual.hpp"
 #include "struct_size.hpp"
 #include "struct_inputs.hpp"
 #include "struct_BC.hpp"
-#include "struct_residual.hpp"
 
 #include "res.cpp"
 
-void ssp_rk2(class_mesh* mesh, class_Q* Qbar, class_Q* Qface_c1, class_Q* Qface_c2, struct_residual* residual, struct_size* size, struct_inputs* inputs, class_flow* freestream, struct_BC* BC) {
+void ssp_rk2(class_mesh* mesh, class_Q* Qbar, class_Q* Qface_c1, class_Q* Qface_c2, class_residual* residual, struct_size* size, struct_inputs* inputs, class_flow* freestream, struct_BC* BC) {
     // Initilize Variables
     class_Q Q_star;
     Q_star.init(size->num_cells, freestream->gamma);
 
     // Compute Time Step
     int c1, c2;
-    double dt[size->num_cells] = {0}, temp;
+    std::vector<double> dt;
+    dt.assign(size->num_cells, 0);
+    double temp;
     for (int idx = 0; idx < size->num_faces; idx ++) {
         c1 = mesh->face_cell1[idx];
         c2 = mesh->face_cell2[idx];
 
-        dt[c1] += (2*inputs->CFL*mesh->cell_vol[c1])/((std::abs(Qface_c1->Vn_avg[idx])+Qface_c1->c_avg[idx])*mesh->face_area[idx]);
+        dt[c1] += (std::abs(Qface_c1->Vn_avg[idx])+Qface_c1->c_avg[idx])*mesh->face_area[idx];
         if (c2 >= 0){
-            dt[c2] += (2*inputs->CFL*mesh->cell_vol[c2])/((std::abs(Qface_c2->Vn_avg[idx])+Qface_c2->c_avg[idx])*mesh->face_area[idx]);
+            dt[c2] += (std::abs(Qface_c2->Vn_avg[idx])+Qface_c2->c_avg[idx])*mesh->face_area[idx];
         }
+    }
+    for (int idx = 0; idx < size->num_cells; idx ++) {
+        dt[idx] = (2*inputs->CFL*mesh->cell_vol[idx])/dt[idx];
     }
 
     // Calcualte Q star
