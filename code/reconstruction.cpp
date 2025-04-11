d4d3219254847e35b5e59b63e03f9dcecd5704e0
[Avery-Hantla] <[averyshantla@gmail.com]>
2025-04-11 09:58:35 -0500

    merged 1st and 2nd order

--- code/reconstruction.cpp
+++ code/reconstruction.cpp
@@ -17,119 +17,60 @@ void reconstruction(class_mesh* mesh, class_Q* Qbar, class_Q* Qfaces_c1, class_Q
     double Vn;
     class_flow wall_BC;
     wall_BC.gamma = freestream->gamma;
-    switch (inputs->order) {
-        case 1: // First Order FV Scheme
-            for (int idx = 0; idx < size->num_faces; idx ++) {
-                cell_1 = mesh->face_cell1[idx];
 
-                Qfaces_c1->p1[idx] = Qbar->p1[cell_1];
-                Qfaces_c1->p2[idx] = Qbar->p2[cell_1];
-                Qfaces_c1->p3[idx] = Qbar->p3[cell_1];
-                Qfaces_c1->p4[idx] = Qbar->p4[cell_1];
+    // Reconstruct Faces
+    for (int idx = 0; idx < size->num_faces; idx ++) {
+        cell_1 = mesh->face_cell1[idx];
+        cell_2 = mesh->face_cell2[idx];
 
-                cell_2 = mesh->face_cell2[idx];
-                // Apply BC
-                switch (cell_2) { 
-                    case -1: // Freestream BC
-                        Qfaces_c2->p1[idx] = freestream->Q1;
-                        Qfaces_c2->p2[idx] = freestream->Q2;
-                        Qfaces_c2->p3[idx] = freestream->Q3;
-                        Qfaces_c2->p4[idx] = freestream->Q4;
-                        break;
-                    case -2:  // Wall BC
-                        wall_BC.rho = Qbar->rho[cell_1];
-                        wall_BC.P = Qbar->P[cell_1];
-                        Vn = Qbar->u[cell_1]*mesh->face_nx[idx] + Qbar->v[cell_1]*mesh->face_ny[idx];
-                        wall_BC.u = Qbar->u[cell_1] - 2.0*Vn*mesh->face_nx[idx];
-                        wall_BC.v = Qbar->v[cell_1] - 2.0*Vn*mesh->face_ny[idx];
-                        wall_BC.updateQ();
+        Qfaces_c1->p1[idx] = Qbar->p1[cell_1] + Qbar->Qxp1[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp1[cell_1]*mesh->face_cell1_dy[idx];
+        Qfaces_c1->p2[idx] = Qbar->p2[cell_1] + Qbar->Qxp2[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp2[cell_1]*mesh->face_cell1_dy[idx];
+        Qfaces_c1->p3[idx] = Qbar->p3[cell_1] + Qbar->Qxp3[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp3[cell_1]*mesh->face_cell1_dy[idx];
+        Qfaces_c1->p4[idx] = Qbar->p4[cell_1] + Qbar->Qxp4[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp4[cell_1]*mesh->face_cell1_dy[idx];
 
-                        Qfaces_c2->p1[idx] = wall_BC.Q1;
-                        Qfaces_c2->p2[idx] = wall_BC.Q2;
-                        Qfaces_c2->p3[idx] = wall_BC.Q3;
-                        Qfaces_c2->p4[idx] = wall_BC.Q4;
-                        break;
-                    case -3: // Extrapolation BC
-                        Qfaces_c2->p1[idx] = Qbar->p1[cell_1];
-                        Qfaces_c2->p2[idx] = Qbar->p2[cell_1];
-                        Qfaces_c2->p3[idx] = Qbar->p3[cell_1];
-                        Qfaces_c2->p4[idx] = Qbar->p4[cell_1];
-                        break;
-                    default:
-                        Qfaces_c2->p1[idx] = Qbar->p1[cell_2];
-                        Qfaces_c2->p2[idx] = Qbar->p2[cell_2];
-                        Qfaces_c2->p3[idx] = Qbar->p3[cell_2];
-                        Qfaces_c2->p4[idx] = Qbar->p4[cell_2];
-                        break;
-                }
-            }
-            Qfaces_c1->updateflow();
-            Qfaces_c2->updateflow();
-            break;
+        Qfaces_c1->updateflow_int(idx);
+        
+        // Apply BC
+        switch (cell_2) { 
+            case -1: // Freestream BC
+                Qfaces_c2->p1[idx] = freestream->Q1;
+                Qfaces_c2->p2[idx] = freestream->Q2;
+                Qfaces_c2->p3[idx] = freestream->Q3;
+                Qfaces_c2->p4[idx] = freestream->Q4;
+                break;
+            case -2:  // Wall BC
+                wall_BC.rho = Qfaces_c1->rho[idx];
+                wall_BC.P = Qfaces_c1->P[idx];
+                Vn = Qfaces_c1->u[idx]*mesh->face_nx[idx] + Qfaces_c1->v[idx]*mesh->face_ny[idx];
+                wall_BC.u = Qfaces_c1->u[idx] - 2.0*Vn*mesh->face_nx[idx];
+                wall_BC.v = Qfaces_c1->v[idx] - 2.0*Vn*mesh->face_ny[idx];
+                wall_BC.updateQ();
 
-        case 2: // Second Order FV Scheme
-            compute_gradient(Qbar, mesh, freestream, size, inputs);
+                Qfaces_c2->p1[idx] = wall_BC.Q1;
+                Qfaces_c2->p2[idx] = wall_BC.Q2;
+                Qfaces_c2->p3[idx] = wall_BC.Q3;
+                Qfaces_c2->p4[idx] = wall_BC.Q4;
+                break;
+            case -3: // Extrapolation BC
+                Qfaces_c2->p1[idx] = Qfaces_c1->p1[idx];
+                Qfaces_c2->p2[idx] = Qfaces_c1->p2[idx];
+                Qfaces_c2->p3[idx] = Qfaces_c1->p3[idx];
+                Qfaces_c2->p4[idx] = Qfaces_c1->p4[idx];
+                break;
+            default:
+                Qfaces_c2->p1[idx] = Qbar->p1[cell_2] + Qbar->Qxp1[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp1[cell_2]*mesh->face_cell2_dy[idx];
+                Qfaces_c2->p2[idx] = Qbar->p2[cell_2] + Qbar->Qxp2[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp2[cell_2]*mesh->face_cell2_dy[idx];
+                Qfaces_c2->p3[idx] = Qbar->p3[cell_2] + Qbar->Qxp3[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp3[cell_2]*mesh->face_cell2_dy[idx];
+                Qfaces_c2->p4[idx] = Qbar->p4[cell_2] + Qbar->Qxp4[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp4[cell_2]*mesh->face_cell2_dy[idx];
+                break;
+        }
+    }
+    Qfaces_c2->updateflow();
 
-            // Reconstruct Faces
-            for (int idx = 0; idx < size->num_faces; idx ++) {
-                cell_1 = mesh->face_cell1[idx];
-                cell_2 = mesh->face_cell2[idx];
-
-                Qfaces_c1->p1[idx] = Qbar->p1[cell_1] + Qbar->Qxp1[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp1[cell_1]*mesh->face_cell1_dy[idx];
-                Qfaces_c1->p2[idx] = Qbar->p2[cell_1] + Qbar->Qxp2[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp2[cell_1]*mesh->face_cell1_dy[idx];
-                Qfaces_c1->p3[idx] = Qbar->p3[cell_1] + Qbar->Qxp3[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp3[cell_1]*mesh->face_cell1_dy[idx];
-                Qfaces_c1->p4[idx] = Qbar->p4[cell_1] + Qbar->Qxp4[cell_1]*mesh->face_cell1_dx[idx] + Qbar->Qyp4[cell_1]*mesh->face_cell1_dy[idx];
-
-                Qfaces_c1->updateflow_int(idx);
-
-                cell_2 = mesh->face_cell2[idx];
-                // Apply BC
-                switch (cell_2) { 
-                    case -1: // Freestream BC
-                        Qfaces_c2->p1[idx] = freestream->Q1;
-                        Qfaces_c2->p2[idx] = freestream->Q2;
-                        Qfaces_c2->p3[idx] = freestream->Q3;
-                        Qfaces_c2->p4[idx] = freestream->Q4;
-                        break;
-                    case -2:  // Wall BC
-                        wall_BC.rho = Qfaces_c1->rho[idx];
-                        wall_BC.P = Qfaces_c1->P[idx];
-                        Vn = Qfaces_c1->u[idx]*mesh->face_nx[idx] + Qfaces_c1->v[idx]*mesh->face_ny[idx];
-                        wall_BC.u = Qfaces_c1->u[idx] - 2.0*Vn*mesh->face_nx[idx];
-                        wall_BC.v = Qfaces_c1->v[idx] - 2.0*Vn*mesh->face_ny[idx];
-                        wall_BC.updateQ();
-
-                        Qfaces_c2->p1[idx] = wall_BC.Q1;
-                        Qfaces_c2->p2[idx] = wall_BC.Q2;
-                        Qfaces_c2->p3[idx] = wall_BC.Q3;
-                        Qfaces_c2->p4[idx] = wall_BC.Q4;
-                        break;
-                    case -3: // Extrapolation BC
-                        Qfaces_c2->p1[idx] = Qfaces_c1->p1[idx];
-                        Qfaces_c2->p2[idx] = Qfaces_c1->p2[idx];
-                        Qfaces_c2->p3[idx] = Qfaces_c1->p3[idx];
-                        Qfaces_c2->p4[idx] = Qfaces_c1->p4[idx];
-                        break;
-                    default:
-                        Qfaces_c2->p1[idx] = Qbar->p1[cell_2] + Qbar->Qxp1[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp1[cell_2]*mesh->face_cell2_dy[idx];
-                        Qfaces_c2->p2[idx] = Qbar->p2[cell_2] + Qbar->Qxp2[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp2[cell_2]*mesh->face_cell2_dy[idx];
-                        Qfaces_c2->p3[idx] = Qbar->p3[cell_2] + Qbar->Qxp3[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp3[cell_2]*mesh->face_cell2_dy[idx];
-                        Qfaces_c2->p4[idx] = Qbar->p4[cell_2] + Qbar->Qxp4[cell_2]*mesh->face_cell2_dx[idx] + Qbar->Qyp4[cell_2]*mesh->face_cell2_dy[idx];
-                        break;
-                }
-            }
-            Qfaces_c2->updateflow();
-
-            // Find Viscous Fluxes for NS
-            switch (inputs->eqn) {
-                case 2: // If eqn == NS
-
-                    break;
-            }
+    // Find Viscous Fluxes for NS
+    switch (inputs->eqn) {
+        case 2: // If eqn == NS
 
             break;
-
-        default:
-            std::cout << "ERROR: ORDER NOT IMPLEMENTED \n";
-    };
+    }
 }
